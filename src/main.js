902f93b907e32a02abddf06ac3ee5d92cbcd6a7e
Candido Souza <candidosouzza@gmail.com>
2020-02-12 13:29:42 -0300

    Listando coleções com v-for

--- src/main.js
+++ src/main.js
@@ -4,6 +4,10 @@ import { Time } from './time'
 new Vue({
   el: '#app',
   data: {
-    time: new Time("América MG", require('./assets/atletico_mg_60x60.png'))
+    times: [
+      new Time("América MG", require('./assets/america_mg_60x60.png')),
+      new Time("Atletico MG", require('./assets/atletico_mg_60x60.png')),
+      new Time("América MG", require('./assets/atletico-pr_60x60.png')),
+    ]
   }
 })
