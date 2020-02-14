dce121082985632202030c64fedd4aa4fe13ac3a
Candido Souza <candidosouzza@gmail.com>
2020-02-14 11:19:12 -0300

    Configurando as condicionais para a view

--- src/main.js
+++ src/main.js
@@ -1,36 +1,34 @@
-import Vue from 'vue';
-import { Time } from './time'
+import Vue from "vue";
+import { Time } from "./time";
 
-require('bootstrap/dist/css/bootstrap.min.css')
-require('bootstrap')
+require("bootstrap/dist/css/bootstrap.min.css");
+require("bootstrap");
 
-new Vue({
-  el: '#app',
+var teste = new Vue({
+  el: "#app",
   data: {
-    colunas: [
-      'nome', 'pontos', 'gm', 'gs', 'saldo', 
-    ],
+    colunas: ["nome", "pontos", "gm", "gs", "saldo"],
     times: [
-      new Time('Palmeiras', require('./assets/palmeiras_60x60.png')),
-      new Time('Flamengo', require('./assets/flamengo_60x60.png')),
-      new Time('Atlético-MG', require('./assets/atletico_mg_60x60.png')),
-      new Time('Santos', require('./assets/santos_60x60.png')),
-      new Time('Botafogo', require('./assets/botafogo_60x60.png')),
-      new Time('Atlético-PR', require('./assets/atletico-pr_60x60.png')),
-      new Time('Corinthians', require('./assets/corinthians_60x60.png')),
-      new Time('Grêmio', require('./assets/gremio_60x60.png')),
-      new Time('Fluminense', require('./assets/fluminense_60x60.png')),
-      new Time('Ponte Preta', require('./assets/ponte_preta_60x60.png')),
-      new Time('Chapecoense', require('./assets/chapecoense_60x60.png')),
-      new Time('São Paulo', require('./assets/sao_paulo_60x60.png')),
-      new Time('Cruzeiro', require('./assets/cruzeiro_60x60.png')),
-      new Time('Sport', require('./assets/sport_60x60.png')),
-      new Time('Coritiba', require('./assets/coritiba_60x60.png')),
-      new Time('Internacional', require('./assets/internacional_60x60.png')),
-      new Time('Vitória', require('./assets/vitoria_60x60.png')),
-      new Time('Figueirense', require('./assets/figueirense_60x60.png')),
-      new Time('Santa Cruz', require('./assets/santa_cruz_60x60.png')),
-      new Time('América-MG', require('./assets/america_mg_60x60.png')),
+      new Time("Palmeiras", require("./assets/palmeiras_60x60.png")),
+      new Time("Flamengo", require("./assets/flamengo_60x60.png")),
+      new Time("Atlético-MG", require("./assets/atletico_mg_60x60.png")),
+      new Time("Santos", require("./assets/santos_60x60.png")),
+      new Time("Botafogo", require("./assets/botafogo_60x60.png")),
+      new Time("Atlético-PR", require("./assets/atletico-pr_60x60.png")),
+      new Time("Corinthians", require("./assets/corinthians_60x60.png")),
+      new Time("Grêmio", require("./assets/gremio_60x60.png")),
+      new Time("Fluminense", require("./assets/fluminense_60x60.png")),
+      new Time("Ponte Preta", require("./assets/ponte_preta_60x60.png")),
+      new Time("Chapecoense", require("./assets/chapecoense_60x60.png")),
+      new Time("São Paulo", require("./assets/sao_paulo_60x60.png")),
+      new Time("Cruzeiro", require("./assets/cruzeiro_60x60.png")),
+      new Time("Sport", require("./assets/sport_60x60.png")),
+      new Time("Coritiba", require("./assets/coritiba_60x60.png")),
+      new Time("Internacional", require("./assets/internacional_60x60.png")),
+      new Time("Vitória", require("./assets/vitoria_60x60.png")),
+      new Time("Figueirense", require("./assets/figueirense_60x60.png")),
+      new Time("Santa Cruz", require("./assets/santa_cruz_60x60.png")),
+      new Time("América-MG", require("./assets/america_mg_60x60.png"))
     ],
 
     novoJogo: {
@@ -43,16 +41,8 @@ new Vue({
         gols: 0
       }
     },
-  },
 
-  created() {
-    let indexCasa = Math.floor(Math.random() * 20),
-        indexFora = Math.floor(Math.random() * 20);
-
-    this.novoJogo.casa.time = this.times[indexCasa];
-    this.novoJogo.casa.gols = 0;
-    this.novoJogo.fora.time = this.times[indexFora];
-    this.novoJogo.fora.gols = 0;
+    view: "tabela"
   },
 
   methods: {
@@ -61,7 +51,22 @@ new Vue({
       let gols = +this.novoJogo.casa.gols;
       let golsAdversario = +this.novoJogo.fora.gols;
       this.novoJogo.casa.time.fimJogo(timeAdversario, gols, golsAdversario);
+      this.showView("tabela");
     },
+    createNovoJogo() {
+      let indexCasa = Math.floor(Math.random() * 20),
+        indexFora = Math.floor(Math.random() * 20);
+
+      this.novoJogo.casa.time = this.times[indexCasa];
+      this.novoJogo.casa.gols = 0;
+      this.novoJogo.fora.time = this.times[indexFora];
+      this.novoJogo.fora.gols = 0;
+      this.showView("novojogo");
+    },
+
+    showView(view) {
+      this.view = view;
+    }
   },
 
   filters: {
@@ -71,8 +76,8 @@ new Vue({
 
     ucwords(value) {
       return value.charAt(0).toUpperCase() + value.slice(1);
-    },
-  },
+    }
+  }
+});
 
-  
-})
+console.log(teste.view);
