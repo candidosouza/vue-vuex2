6c87450e8f94040963c66e4dbb2fdead3abf52a3
Candido Souza <candidosouzza@gmail.com>
2020-02-14 14:45:10 -0300

    Criando o componente para o app

--- src/main.js
+++ src/main.js
@@ -1,54 +1,59 @@
 import Vue from "vue";
 import { Time } from "./time";
-import _ from 'lodash';
+import _ from "lodash";
 
 require("bootstrap/dist/css/bootstrap.min.css");
 require("bootstrap");
 
-new Vue({
-  el: "#app",
-  data: {
-    order: {
-        keys: ['pontos', 'gm', 'gs'],
-        sort: ['desc', 'desc', 'asc']
-    },
-    filter: '',
-    colunas: ["nome", "pontos", "gm", "gs", "saldo"],
-    times: [
-      new Time("Palmeiras", require("./assets/palmeiras_60x60.png")),
-      new Time("Flamengo", require("./assets/flamengo_60x60.png")),
-      new Time("Atlético-MG", require("./assets/atletico_mg_60x60.png")),
-      new Time("Santos", require("./assets/santos_60x60.png")),
-      new Time("Botafogo", require("./assets/botafogo_60x60.png")),
-      new Time("Atlético-PR", require("./assets/atletico-pr_60x60.png")),
-      new Time("Corinthians", require("./assets/corinthians_60x60.png")),
-      new Time("Grêmio", require("./assets/gremio_60x60.png")),
-      new Time("Fluminense", require("./assets/fluminense_60x60.png")),
-      new Time("Ponte Preta", require("./assets/ponte_preta_60x60.png")),
-      new Time("Chapecoense", require("./assets/chapecoense_60x60.png")),
-      new Time("São Paulo", require("./assets/sao_paulo_60x60.png")),
-      new Time("Cruzeiro", require("./assets/cruzeiro_60x60.png")),
-      new Time("Sport", require("./assets/sport_60x60.png")),
-      new Time("Coritiba", require("./assets/coritiba_60x60.png")),
-      new Time("Internacional", require("./assets/internacional_60x60.png")),
-      new Time("Vitória", require("./assets/vitoria_60x60.png")),
-      new Time("Figueirense", require("./assets/figueirense_60x60.png")),
-      new Time("Santa Cruz", require("./assets/santa_cruz_60x60.png")),
-      new Time("América-MG", require("./assets/america_mg_60x60.png"))
-    ],
+let appComponent = Vue.extend({
+  template: `
 
-    novoJogo: {
-      casa: {
-        time: null,
-        gols: 0
+  `,
+
+  data() {
+    return {
+      order: {
+        keys: ["pontos", "gm", "gs"],
+        sort: ["desc", "desc", "asc"]
       },
-      fora: {
-        time: null,
-        gols: 0
-      }
-    },
+      filter: "",
+      colunas: ["nome", "pontos", "gm", "gs", "saldo"],
+      times: [
+        new Time("Palmeiras", require("./assets/palmeiras_60x60.png")),
+        new Time("Flamengo", require("./assets/flamengo_60x60.png")),
+        new Time("Atlético-MG", require("./assets/atletico_mg_60x60.png")),
+        new Time("Santos", require("./assets/santos_60x60.png")),
+        new Time("Botafogo", require("./assets/botafogo_60x60.png")),
+        new Time("Atlético-PR", require("./assets/atletico-pr_60x60.png")),
+        new Time("Corinthians", require("./assets/corinthians_60x60.png")),
+        new Time("Grêmio", require("./assets/gremio_60x60.png")),
+        new Time("Fluminense", require("./assets/fluminense_60x60.png")),
+        new Time("Ponte Preta", require("./assets/ponte_preta_60x60.png")),
+        new Time("Chapecoense", require("./assets/chapecoense_60x60.png")),
+        new Time("São Paulo", require("./assets/sao_paulo_60x60.png")),
+        new Time("Cruzeiro", require("./assets/cruzeiro_60x60.png")),
+        new Time("Sport", require("./assets/sport_60x60.png")),
+        new Time("Coritiba", require("./assets/coritiba_60x60.png")),
+        new Time("Internacional", require("./assets/internacional_60x60.png")),
+        new Time("Vitória", require("./assets/vitoria_60x60.png")),
+        new Time("Figueirense", require("./assets/figueirense_60x60.png")),
+        new Time("Santa Cruz", require("./assets/santa_cruz_60x60.png")),
+        new Time("América-MG", require("./assets/america_mg_60x60.png"))
+      ],
 
-    view: "tabela"
+      novoJogo: {
+        casa: {
+          time: null,
+          gols: 0
+        },
+        fora: {
+          time: null,
+          gols: 0
+        }
+      },
+
+      view: "tabela"
+    };
   },
 
   methods: {
@@ -75,7 +80,7 @@ new Vue({
     },
     sortBy(coluna) {
       this.order.keys = coluna;
-      this.order.sort = this.order.sort == 'desc' ? 'asc' : 'desc';
+      this.order.sort = this.order.sort == "desc" ? "asc" : "desc";
     }
   },
 
@@ -86,7 +91,7 @@ new Vue({
       return _.filter(colecao, item => {
         return item.nome.indexOf(this.filter) >= 0;
       });
-    },
+    }
   },
 
   filters: {
@@ -98,6 +103,10 @@ new Vue({
       return value.charAt(0).toUpperCase() + value.slice(1);
     }
   }
+});
+
+new Vue({
+  el: "#app"
 });
 
 // console.log(teste.view);
